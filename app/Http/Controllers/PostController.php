af3a1a6556ed09fded94f90e2a63bf2498ab8572
ronphobos <sidibearouna71@gmail.com>
2022-04-11 08:43:36 +0000

    ajout fonction controller et route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+     public function index()
+    {
+        $post = Post::create([
+            "name" => "Back to future",
+            "slug" => "laravel-generate-multi-slug-on-load",
+            "description" => "This is just the laravel slug example"
+        ]);
+        dd($post);
+    }
 }
